b8af8ce5b4563f9d66d97ad9e1c9ded7de838df7
wangtao <1259709654@qq.com>
2019-05-28 21:40:48 +0800

    chat对话框调整

--- app/page/Chat.js
+++ app/page/Chat.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, FlatList, TextInput } from 'react-native';
 import fetchRequest from '../utils/fetch'
+import Icon from "react-native-vector-icons/Ionicons";
 
 class ChatScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -44,7 +45,6 @@ class ChatScreen extends React.Component {
     }
 
     render() {
-        let dom = []
         let msgArr = [
           {
             'isMe': false, uri: 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg', 
@@ -72,58 +72,90 @@ class ChatScreen extends React.Component {
           {
             'isMe': true, uri: 'http://www.158pic.com/uploads/allimg/1904/2-1Z4021935240-L.jpg', text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改'
           },
+          {
+            'isMe': true, uri: 'http://www.158pic.com/uploads/allimg/1904/2-1Z4021935240-L.jpg', 
+            text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改'
+          },
+          {
+            'isMe': false, uri: 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg', text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改 图像上特殊的色彩，改变不透明像素的颜色'
+          },
+          {
+            'isMe': true, uri: 'http://www.158pic.com/uploads/allimg/1904/2-1Z4021935240-L.jpg', text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改'
+          },
+          {
+            'isMe': true, uri: 'http://www.158pic.com/uploads/allimg/1904/2-1Z4021935240-L.jpg', 
+            text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改'
+          },
+          {
+            'isMe': false, uri: 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg', text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改 图像上特殊的色彩，改变不透明像素的颜色'
+          },
+          {
+            'isMe': true, uri: 'http://www.158pic.com/uploads/allimg/1904/2-1Z4021935240-L.jpg', text: '像上特殊的色彩，改像上特殊的色彩，改像上特殊的色彩，改'
+          },
         ]
-        for (let i in msgArr) {
-          if(!msgArr[i].isMe) {
-
-          }
-          dom.push(
-              <View  key={msgArr[i].uri} style={{flex: 0,width: '90%', 
-                marginTop: 15,
-                flexDirection: msgArr[i].isMe ? 'row-reverse' : 'row', 
-                justifyContent: msgArr[i].isMe ? 'flex-end' : 'flex-start'
-                }}>
-                <Image source={{uri: msgArr[i].uri, width: 40, height: 40}} />
-                <View style={{}}>
-                    <Text style={{
-                      width: 20,
-                      height: 20,
-                      backgroundColor: msgArr[i].isMe ? '#1AAD19' : '#fff',
-                      position: 'absolute',
-                      top: 10,
-                      left: msgArr[i].isMe ? '91%' : 5,
-                      transform: [{rotate:'45deg'}],
-                    }}>
-                    </Text>
-                    <Text style={{marginLeft: 10, borderRadius: 5, 
-                    marginRight: 10,
-                    minHeight: 40,
-                    padding: 5, backgroundColor: msgArr[i].isMe ? '#1AAD19' : '#fff'}}>
-                      {msgArr[i].text}
-                    </Text>
-                </View>
-            </View>
-          )
-        }
+        
         return (
-          <View style={{flex: 1, padding: 20, paddingTop: 0,backgroundColor: '#eee'}}>
-            {dom}
+          <View style={{flex: 1, padding: 10, paddingTop: 0,backgroundColor: '#eee'}}>
+            <FlatList
+              style={{}}
+              data={msgArr}
+              renderItem= {
+                ({item}) => 
+                  <View style={{
+                    position: 'relative', 
+                    width: item.isMe ? '100%' : '85%', 
+                    marginTop: 10, 
+                    display: 'flex',
+                    flexDirection: item.isMe ? 'row-reverse' : 'row',
+                    borderRadius: 5,
+                    }} key={item.uri}>
+                    <Image source={{uri: item.uri, width: 40, height: 40}}/>
+                    <Text style={{
+                      width: 15,height: 15, 
+                      backgroundColor: item.isMe ? '#1AAD19' : '#fff', 
+                      marginTop: 10,
+                      transform: [{rotate:'45deg'}],
+                      position: 'relative',
+                      left: 8}}></Text>
+                    <Text style={{ 
+                      backgroundColor: item.isMe ? '#1AAD19' : '#fff', 
+                      lineHeight: 20, padding: 5, 
+                      borderRadius: 5,
+                      marginLeft: item.isMe ? 60 : 0
+                      }}>
+                      {item.text}
+                    </Text>
+                  </View>
+              }
+              keyExtractor={(item, index) => item._id}
+            />
+            <View style={styles.inputWrapper}>
+              <Icon name="ios-volume-high" size={20} color="#333" style={{flex: 1}}></Icon>
+              <TextInput
+                style={{height: 40, flex: 8, backgroundColor: '#fff'}}
+                placeholder="我知道你现在在想我"
+                onChangeText={(text) => alert(text)}
+              />
+              <Icon name="md-happy" size={20} color="#333" style={{flex: 1}}></Icon>
+              <Icon name="md-add-circle" size={20} color="#333" style={{flex: 1}}></Icon>
+            </View>
           </View>
+          
         );
     }
 }
 const styles = StyleSheet.create({
-    container: {
-      
-    },
-    Msgarrow: {
-      width: 20,
-      height: 20,
-      backgroundColor: '#1AAD19',
-      position: 'absolute',
-      top: 10,
-      left: 5,
-      transform: [{rotate:'45deg'}],
+    inputWrapper: {
+      height: 50,
+      backgroundColor: '#fff',
+      marginLeft: -10,
+      marginRight: -10,
+      marginBottom: -10,
+      paddingLeft: 15,
+      paddingRight: 15,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center'
     }
   })
 
